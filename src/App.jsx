c1b4911631339728288955cef8f2a9ba7d495ae4
necryl <74096664+Necryl@users.noreply.github.com>
2024-10-30 15:31:58 +0530

    Create cards base code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import Card from "./Card.jsx";
 
 function App() {
-  return <h1>Memory Game</h1>;
+  function renderCards() {
+    let result = [];
+    for (let i = 0; i < 10; i++) {
+      console.log(i);
+      result.push(<Card key={i}>Card {i + 1}</Card>);
+    }
+    console.log(result);
+    return result;
+  }
+  return (
+    <>
+      <h1>Memory Game</h1>
+      <div className="board">{renderCards()}</div>
+    </>
+  );
 }
 
 export default App;
